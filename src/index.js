79186434be7b3d614a66f745364294ebf87914eb
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-08-01 23:54:21 -0300

    Update deploy DB

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './Redux/store';
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:3001/rickandmorty';
-axios.defaults.baseURL = 'https://rickandmorty-back-9ygv.onrender.com//rickandmorty';
+axios.defaults.baseURL = 'https://rickandmorty-back-9ygv.onrender.com/rickandmorty';
 
 createRoot(document.getElementById('root'))
   .render(
